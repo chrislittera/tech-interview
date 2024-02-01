3a624202b3e1f1ed7ce3c8fa5f7cc29d899d1566
Chris Hansen <99918016+chrislittera@users.noreply.github.com>
2024-02-01 09:20:06 -0500

    Update TestTwo.java

--- src/main/java/com/littera/interview/TestTwo.java
+++ src/main/java/com/littera/interview/TestTwo.java
@@ -3,7 +3,7 @@ package com.littera.interview;
 import java.util.List;
 
 public class TestTwo {
-  public static boolean arrayMirrorsInverse(List<Integer> inputArray) {
+  public static Boolean arrayMirrorsInverse(List<Integer> inputArray) {
     return null;
   }
 
